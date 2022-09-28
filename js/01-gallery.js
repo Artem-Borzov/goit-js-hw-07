c17359cdf6d82d834a48cbf11eafd7c1f3004916
Artem-Borzov <abug990@gmail.com>
2022-09-28 12:45:07 +0300

    main-task-1-completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems.map(galleryItemRender);
+
+function galleryItemRender({ preview, original, description }) {
+  return `<div class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`;
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", [...galleryHTML].join(""));
+
+galleryEl.addEventListener("click", onClickCatchURL);
+
+function onClickCatchURL(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source}>`
+  );
+  instance.show();
+}
